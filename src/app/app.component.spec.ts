f646426d39cb0d179f16a9267172b210db6d4599
LoKV <seath@mail.fresnostate.edu>
2019-11-20 13:56:11 -0800

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('contact-app');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('contact-app app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to contact-app!');
   });
 });
---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './contacts.model';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent implements OnInit {
+
+  contacts: Array<Contact> = [];
+  contactParams = '';
+  constructor(private http: Http) { }
+
+  async ngOnInit() {
+    this.loadContacts();
+
+
+  }
+
+  async loadContacts() {
+    const savedContacts = this.getItemsFromLocalStorage('contacts');
+    if (savedContacts.length > 0) {
+      this.contacts = savedContacts;
+
+
+    } else {
+      this.contacts = await this.loadItemsFromFile();
+    }
+    this.sortById(this.contacts);
+  }
+
+  async loadItemsFromFile() {
+    const data = await this.http.get('assets/contacts.json').toPromise();
+    return data.json();
+  }
+
+  addContact() {
+    this.contacts.unshift(new Contact({}));
+
+  }
+
+  delete(index: number) {
+    this.contacts.splice(index, 1);
+    this.saveItemsToLocalStorage(this.contacts);
+  }
+
+  save(contacts: any) {
+    this.sortById(this.contacts);
+    contacts.editing = false;
+
+  }
+  saveItemsToLocalStorage(contacts: Array<Contact>) {
+    const savedContacts = localStorage.setItem('contacts', JSON.stringify(contacts));
+    return savedContacts;
+  }
+
+  getItemsFromLocalStorage(key: string) {
+    const savedContacts = JSON.parse(localStorage.getItem(key));
+    return savedContacts;
+
+  }
+
+  search(params: string) {
+
+    this.contacts = this.contacts.filter((item: Contact) => {
+      const fullName = item.firstName + ' ' + item.lastName;
+      if (params === fullName || params === item.firstName || params === item.lastName) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  sortById(contacts: Array<Contact>) {
+    contacts.sort((a: Contact, b: Contact) => {
+
+      return a.id > b.id ? 1 : -1;
+    });
+  }
+
+
+}
